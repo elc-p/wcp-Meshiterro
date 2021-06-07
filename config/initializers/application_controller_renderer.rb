e526b2da6a07ba33e425c013256f9cf70f402ffa
elc-p <yomtype10@gmail.com>
2021-06-07 02:21:15 +0000

    [Start]4

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '626863e063edd304017283a4ea3fd9c6182376f0bf4ae4ddfb796abd60641238f76cc1e6f32fe12436a20b73598d04afe38c491cd751ecf20125e773384fabf2'
